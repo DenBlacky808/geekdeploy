4a23b05e63ef4312b80438d0d548c4c42f17d8a5
DenisChernyavskiy <92929@mail.ru>
2021-09-01 20:35:54 +0300

    lesson_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    name = {'title': 'Магазин'}
+    return render(request, 'mainapp/index.html', name)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    name = {'title': 'Каталог'}
+    return render(request, 'mainapp/products.html', name)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    name = {'title': 'Контакты'}
+    return render(request, 'mainapp/contact.html', name)
+
+
