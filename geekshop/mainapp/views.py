390425c64ce3fdb42ebd6970417eb1b6715a3e39
DenisChernyavskiy <92929@mail.ru>
2021-09-13 19:26:12 +0300

    lesson_5 product_list and basket

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
+
 
 
 def main(request):
@@ -8,10 +9,46 @@ def main(request):
 
 
 def products(request, pk=None):
-    categories = ProductCategory.objects.all()[:4]
-    products = Product.objects.all()[:4]
-    content = {'title': 'Каталог', 'products': products, 'pk': pk, 'categories': categories}
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        links_menu = ProductCategory.objects.all()[:4]
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+
     return render(request, 'mainapp/products.html', content)
+
+
+# def products(request, pk=None):
+#     categories = ProductCategory.objects.all()[:4]
+#     products = Product.objects.all()[:4]
+#     content = {'title': 'Каталог', 'products': products, 'pk': pk, 'categories': categories}
+#     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
