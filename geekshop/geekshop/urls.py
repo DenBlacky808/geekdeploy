9a3d52c00e87eb24bce799dbc48bc7b559a57560
DenisChernyavskiy <92929@mail.ru>
2021-09-20 22:54:40 +0300

    lesson_7 start

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -22,12 +22,13 @@ from django.conf.urls import include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
     path('', mainapp.main, name='main'),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
