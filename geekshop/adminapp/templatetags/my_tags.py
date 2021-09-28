ade4ffc1235b92e3684aebafa3eaf01b0b58f211
Denis <247300@gmail.com>
2021-09-28 19:40:45 +0300

    lesson_8

--- geekshop/adminapp/templatetags/my_tags.py
+++ geekshop/adminapp/templatetags/my_tags.py
@@ -5,10 +5,6 @@ register = template.Library()
 
 
 def media_folder_products(string):
-    """
-    Автоматически добавляет относительный URL-путь к медиафайлам продуктов
-    products_images/product1.jpg --> /media/products_images/product1.jpg
-    """
     if not string:
         string = 'products_images/default.jpg'
 
@@ -17,10 +13,6 @@ def media_folder_products(string):
 
 @register.filter(name='media_folder_users')
 def media_folder_users(string):
-    """
-    Автоматически добавляет относительный URL-путь к медиафайлам пользователей
-    users_avatars/user1.jpg --> /media/users_avatars/user1.jpg
-    """
     if not string:
         string = 'users_avatars/default.jpg'
 
