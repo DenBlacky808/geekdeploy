93849b32ee549cdcb55c1b02f649d61e9512cfdc
Denis <247300@gmail.com>
2021-10-21 18:48:03 +0300

    fix debug_toolbar

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -163,7 +163,7 @@ if DEBUG:
        'debug_toolbar.panels.logging.LoggingPanel',
        'debug_toolbar.panels.redirects.RedirectsPanel',
        'debug_toolbar.panels.profiling.ProfilingPanel',
-       'template_profiler_panel.panels.template.TemplateProfilerPanel',
+       # 'template_profiler_panel.panels.template.TemplateProfilerPanel',
    ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
